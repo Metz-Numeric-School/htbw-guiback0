5fb2339777a943723a9b417e5c4ae9b563be0beb
guiback0 <guillaume.zimmer.pro@gmail.com>
2026-02-02 11:45:29 +0100

    fix: member dashboard

--- templates/member/dashboard/index.html.php
+++ templates/member/dashboard/index.html.php
@@ -1,7 +1,7 @@
 <?php $layout = 'base.html.php'; ?>
 
 <div class="container py-5">
-    <h1 class="mb-4">Bonjour <?= $_SESSION['user']['firstname'] ?> !</h1>
+    <h1 class="mb-4">Bonjour <?= $_SESSION['user']['username'] ?> !</h1>
 
     <div class="row mb-4">
         <!-- Statistiques rapides -->
